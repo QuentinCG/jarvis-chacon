f5183eecc52e14a2057af4d3d800e92304df5b67
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-28 15:26:54 +0200

    function: Get ride of config plugin path

--- functions.sh
+++ functions.sh
@@ -13,7 +13,8 @@ pg_chacon_turn () {
     if [[ "$order" =~ .*$sdevice.* ]]; then
       local address="$(echo $pg_chacon_config | jq -r ".devices[] | select(.name==\"$device\") | .address")"
       say "$(pg_chacon_lg "switching_$1" "$2")"
-      local cmd="sudo ./$pg_chacon_dir/chacon_send 0 $pg_chacon_num $address $1"
+      local dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+      local cmd="sudo ./$dir/chacon_send 0 $pg_chacon_num $address $1"
       $verbose && jv_debug "$> $cmd"
       eval $cmd | while read line; do jv_debug "$line"; done
       say "$(pg_chacon_lg "switching_$1_done" "$2")"
