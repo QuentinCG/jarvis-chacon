890fd96f553e05aafca1c74c79c50473a0124f6c
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-28 15:21:00 +0200

    plugin: Improve base function

--- functions.sh
+++ functions.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Turn chacon device on/off
+#
+# $1: action [on|off]
+# $2: device name (name of the device to activate/deactivate)
+#
+# return: 0 if success, else > 0
 pg_chacon_turn () {
-    # $1: action [on|off]
-    # $2: received order
-    local -r order="$(jv_sanitize "$2")"
-    while read device; do
-        local sdevice="$(jv_sanitize "$device" ".*")"
-        if [[ "$order" =~ .*$sdevice.* ]]; then
-            local address="$(echo $pg_chacon_config | jq -r ".devices[] | select(.name==\"$device\") | .address")"
-            say "$(pg_chacon_lg "switching_$1" "$2")"
-            local cmd="sudo ./$pg_chacon_dir/chacon_send 0 $pg_chacon_num $address $1"
-            $verbose && jv_debug "$> $cmd"
-            eval $cmd | while read line; do jv_debug "$line"; done # safe as no user input in $cmd. pass each output line to jv_debu
-			say "c'est fait"
-            return $?
-        fi
-    done <<< "$(echo $pg_chacon_config | jq -r '.devices[].name')"
-    say "$(pg_chacon_lg "no_device_matching" "$2")"
-    return 1
+  local -r order="$(jv_sanitize "$2")"
+  while read device; do
+    local sdevice="$(jv_sanitize "$device" ".*")"
+    if [[ "$order" =~ .*$sdevice.* ]]; then
+      local address="$(echo $pg_chacon_config | jq -r ".devices[] | select(.name==\"$device\") | .address")"
+      say "$(pg_chacon_lg "switching_$1" "$2")"
+      local cmd="sudo ./$pg_chacon_dir/chacon_send 0 $pg_chacon_num $address $1"
+      $verbose && jv_debug "$> $cmd"
+      eval $cmd | while read line; do jv_debug "$line"; done
+      say "$(pg_chacon_lg "switching_$1_done" "$2")"
+      return $?
+    fi
+  done <<< "$(echo $pg_chacon_config | jq -r '.devices[].name')"
+  say "$(pg_chacon_lg no_device_matching "$2")"
+  return 1
 }
