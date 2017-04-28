2039f2ebdc67c718ebeb2a1c86167cb402ac3202
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-28 15:23:21 +0200

    plugin: Update french translation

--- fr/functions.sh
+++ fr/functions.sh
@@ -1,8 +1,10 @@
 pg_chacon_lg() {
-    case "$1" in
-        no_device_matching) echo "Aucun appareil ne correspond à $2";;
-        switching_on) echo "J'allume $2";;
-        switching_off) echo "J'éteins $2";;
-        *) jv_error "ERROR: unknown translation key: $1";;
-    esac
+  case "$1" in
+    no_device_matching) echo "Aucun appareil ne correspond à $2";;
+    switching_on) echo "J'allume $2";;
+    switching_off) echo "J'éteins $2";;
+    switching_on_done) echo "$2 allumé";;
+    switching_off_done) echo "$2 éteint";;
+    *) jv_error "Erreur: Clef de traduction '$2' non reconnue. Merci de contacter le concepteur de ce plugin.";;
+  esac
 }
