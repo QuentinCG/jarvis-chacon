f41c414d742e3aad17cf6f31aed2664704a0098f
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-28 13:00:45 +0200

    install: Add proper install (wiring pi, compile the program)

--- install.sh
+++ install.sh
@@ -1,2 +1,25 @@
-#!/bin/bash
-# Use only if you need to perform changes on the user system such as installing software
+#!/usr/bin/env bash
+
+# Used tutorials:
+#  - Wiring Pi install: http://wiringpi.com/download-and-install/
+#  - Chacon sender program: https://arno0x0x.wordpress.com/2015/04/02/rf433-outlet/
+
+# Check if Wiring Pi already installed
+result_wiring_pi_installed=`gpio -v`
+if [[ ! $result =~ "gpio version:" ]]; then
+  # Delete potential previous version of Wiring Pi (this should never happens)
+  sudo apt-get purge wiringpi
+  hash -r
+
+  # Install last version of Wiring Pi
+  rm -rf /tmp/wiringPi
+  git clone git://git.drogon.net/wiringPi /tmp/wiringPi
+  /tmp/wiringPi/build
+  rm -rf /tmp/wiringPi
+fi
+
+# Install G++ (compiler)
+[[ -z $(which g++) ]] && sudo apt-get --yes --force-yes install g++
+
+# Compile "chacon_send" program
+g++ -o chacon_send chacon_send.cpp -lwiringPi
