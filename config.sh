0bd8a59a7d5b19932afdd2b04e44a27c7f0b32e0
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-01 18:06:36 +0200

    config: Improve config

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
-pg_chacon_num=12325261
+pg_chacon_num=12345678 # 12345678 for DIO 54760 remote control, 12325261 for other DIO remote control (or any other number if you don't use the remote controler)
 pg_chacon_config='{ "devices":[
-    { "name": "SALON", "address": "1"},
-    { "name": "SALLE A MANGER", "address": "2"},
-    { "name": "CUISINE", "address": "3"}
+    { "name": "SALON", "address": "0"},
+    { "name": "SALLE A MANGER", "address": "1"},
+    { "name": "CUISINE", "address": "2"}
 ]}'
