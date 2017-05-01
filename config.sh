c7849346912618430129ca5812778bbd405dd500
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-01 18:39:55 +0200

    plugin: Make GPIO pin configurable

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
+pg_chacon_gpio_pin=0 # GPIO PIN number (don't touch if you followed the README explanation)
 pg_chacon_num=12345678 # 12345678 for DIO 54760 remote control, 12325261 for other DIO remote control (or any other number if you don't use the remote controler)
 pg_chacon_config='{ "devices":[
     { "name": "SALON", "address": "0"},
